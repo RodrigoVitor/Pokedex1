aff5402740e74071ca34183fa414fbf9c1970786
RodrigoVitor <rodrigovitorsc2001@gmail.com>
2021-04-29 07:59:46 -0300

    Pokemon

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
-    <div v-for="(poke, index) in pokemons" :key="index">
-      <Pokemon :num="index" :name="poke.name" :url="poke.url"/>
-    </div>
+    <div class="column is-half is-offset-one-quarter"> 
+      <div v-for="(poke, index) in pokemons" :key="index">
+          <Pokemon :num="index" :name="poke.name" :url="poke.url"/>
+      </div>
+    </div>   
   </div>
 </template>
 
--- pokedex/src/components/Pokemon.vue
+++ pokedex/src/components/Pokemon.vue
@@ -1,37 +1,55 @@
 <template>
-    <div>
-        <p>{{num}} {{name | upper}}</p>
-        <small> {{url}} </small>
+  <div>
+    <div class="card">
+      <div class="card-image">
+        <figure>
+          <img :src="pokemon.front" alt="Placeholder image" />
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">{{ num }} - {{ name | upper }}</p>
+            <p class="subtitle is-6">{{ pokemon.type }}</p>
+          </div>
+        </div>
+
+        <div class="content"></div>
+      </div>
     </div>
+  </div>
 </template>
 
-
 <script>
-import axios from "axios"
+import axios from "axios";
 export default {
-    props: {
-        num: Number,
-        name: String,
-        url: String
+  props: {
+    num: Number,
+    name: String,
+    url: String,
+  },
+  filters: {
+    upper: function(value) {
+      let newName = value[0].toUpperCase() + value.slice(1);
+      return newName;
     },
-    filters: {
-        upper:  function(value) {
-            let newName = value[0].toUpperCase() + value.slice(1)
-            return newName
-          }
-    },
-    created: function() {
-        axios.get(this.url).then(res => {
-            this.pokemon.type = res.data.types[0].type.name
-            this.pokemon.front = res.data.sprites.front_default
-            this.pokemon.back = res.data.sprites.back_default
-            console.log(this.pokemon)
-        })
-    },
-    data() {
-        return {
-            pokemon: {}
-        }
-    }
-}
+  },
+  created: function() {
+    axios.get(this.url).then((res) => {
+      this.pokemon.type = res.data.types[0].type.name;
+      this.pokemon.front = res.data.sprites.front_default;
+      this.pokemon.back = res.data.sprites.back_default;
+      console.log(this.pokemon);
+    });
+  },
+  data() {
+    return {
+      pokemon: {
+        type: "",
+        front: "",
+        back: "",
+      },
+    };
+  },
+};
 </script>
