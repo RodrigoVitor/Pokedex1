6f4e5fb28f0b33190fe4e919a01cd09082b78901
RodrigoVitor <rodrigovitorsc2001@gmail.com>
2021-04-30 08:55:24 -0300

    Finalizando projeto

--- pokedex/src/App.vue
+++ pokedex/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
     <div class="column is-half is-offset-one-quarter"> 
-      <div v-for="(poke, index) in pokemons" :key="index">
+      <h4 class="is-size-2"> Pokedex </h4>
+      <input type="text" placeholder="Buscar pokemon" class="input is-rounded" v-model="busca">
+      <button  class="button is-success is-fullwidth" id="buscaBtn" @click="buscar"> Buscar </button>
+      <div v-for="(poke, index) in filterPoke" :key="index">
           <Pokemon :num="index" :name="poke.name" :url="poke.url"/>
       </div>
     </div>   
@@ -16,19 +19,34 @@ export default {
   name: 'App',
   data() {
     return {
-      pokemons: []
+      pokemons: [],
+      filterPoke: [],
+      busca: ""
     }
   },
   created: function() {
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=150&offset=0").then(res => {
       console.log("Pegou as listas")
       this.pokemons = res.data.results
+      this.filterPoke = res.data.results
     })
 
   },
   components: {
     Pokemon
-  }
+  },
+  methods: {
+    buscar: function() {
+      this.filterPoke = this.pokemons
+      if(this.busca == "" || this.busca == " ")
+      {
+         this.filterPoke = this.pokemons
+      } else {
+         this.filterPoke = this.pokemons.filter(pokemon => pokemon.name == this.busca)
+      }
+    }
+  },
+ 
 }
 </script>
 
@@ -40,5 +58,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#buscaBtn {
+  margin-top:10px;
 }
 </style>
